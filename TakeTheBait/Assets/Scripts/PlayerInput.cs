a9214ad24abade2ce46b35e3a819e44056bf7207
Courtney Evins <koinuevins@gmail.com>
2023-10-12 13:15:58 -0500

    fishing rod makes sound when it hits water

--- TakeTheBait/Assets/Scripts/PlayerInput.cs
+++ TakeTheBait/Assets/Scripts/PlayerInput.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] GameObject player;
-    //[SerializeField] GameObject fishingRod;
-    [SerializeField] fishingMechanic fishingStart;
+    [SerializeField] GameObject fishingRod;
+    Score playerScore;
+    GameObject Canvas;
+    bool rodOut = false;
+
+    void Awake(){
+        Canvas = GameObject.Find("Canvas");
+        playerScore = Canvas.GetComponentInChildren<Score>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +26,29 @@ public class PlayerInput : MonoBehaviour
     {
         if((player.transform.position.y < -10.59042)){
 
-            fishingStart.Start();
+            fishingGameStart();
         }
 
+    }
+
+    public void fishingGameStart(){
+        StartCoroutine(fishingGameStartRoutine());
+
+        IEnumerator fishingGameStartRoutine(){
+            if(Input.GetKeyDown(KeyCode.E)){
+                if(rodOut == false){
+                    rodOut = true;
+                    fishingRod.GetComponent<SpriteRenderer>().enabled = true;
+                    fishingRod.GetComponent<AudioSource>().Play();
+                }else{
+                    rodOut = false;
+                    playerScore.ScoreIncrease();
+                    fishingRod.GetComponent<SpriteRenderer>().enabled = false;
+                }
+
+            }
+
+            yield return null;
+        }
     }
 }
