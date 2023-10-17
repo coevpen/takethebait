a9d74059a4b17d8305af01de688700bee09bcee1
Courtney Evins <koinuevins@gmail.com>
2023-10-17 13:31:46 -0500

    fixed resolution and pause with new shark movement

--- TakeTheBait/Assets/Scripts/SharkMovement.cs
+++ TakeTheBait/Assets/Scripts/SharkMovement.cs
@@ -19,7 +19,7 @@ public class SharkMovement : MonoBehaviour
         //if the rod is in the water, the shark moves towards it
         if(fishingRod.GetComponent<SpriteRenderer>().enabled == true){
             Vector3 vel = Vector3.Normalize(fishingRod.transform.position - transform.position);
-            transform.position += vel * speed;
+            transform.position += (vel * speed * Time.deltaTime) * 20;
         }
         else{//otherwise the shark swims off to the side of the screen
            transform.position += new Vector3((speed * Time.deltaTime) * 20,0,0);
