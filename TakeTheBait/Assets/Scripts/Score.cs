18e31d839bb5dac2854cd44180326cb7eb35108c
Courtney Evins <koinuevins@gmail.com>
2023-11-05 16:13:11 -0600

    added high score

--- TakeTheBait/Assets/Scripts/Score.cs
+++ TakeTheBait/Assets/Scripts/Score.cs
@@ -8,15 +8,44 @@ public class Score : MonoBehaviour
 {
     //Text scoreTxt;
     public TMPro.TextMeshProUGUI scoreTxt;
+    public TMPro.TextMeshProUGUI hiScoreTxt;
     int scoreCount = 0;
+    int highScore = 0;
+    int newScore = 0;
+
+    void Awake(){
+        hiScoreTxt = GetComponent<TextMeshProUGUI>();
+        if(PlayerPrefs.HasKey("currentScore")){
+            newScore = PlayerPrefs.GetInt("currentScore");
+        }
+        if(PlayerPrefs.HasKey("hiScore")){
+            if(newScore > PlayerPrefs.GetInt("hiScore")){
+                highScore = newScore;
+                PlayerPrefs.SetInt("hiScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }else{   
+            if(newScore > highScore){
+                highScore = newScore;
+                PlayerPrefs.SetInt("hiScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
+               
+    }
 
     void Start(){
         scoreTxt = GetComponent<TextMeshProUGUI>();
+        hiScoreTxt.text = PlayerPrefs.GetInt("hiScore",0).ToString();
     }
 
     public void ScoreIncrease(int val){
         scoreCount += val;
         scoreTxt.text = scoreCount.ToString();
+        PlayerPrefs.SetInt("currentScore", scoreCount);
+        PlayerPrefs.Save();
     }
+
+
 
 }
