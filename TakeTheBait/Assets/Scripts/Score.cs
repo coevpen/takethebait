6f63eee1f9038138e5deae17ca2453c17c5fbcb6
Courtney Evins <koinuevins@gmail.com>
2023-11-20 15:20:10 -0600

    added high score and can be reset

--- TakeTheBait/Assets/Scripts/Score.cs
+++ TakeTheBait/Assets/Scripts/Score.cs
@@ -8,19 +8,39 @@ public class Score : MonoBehaviour
 {
     //Text scoreTxt;
     public TMPro.TextMeshProUGUI scoreTxt;
+    public TMPro.TextMeshProUGUI highScoreTxt;
+    GameObject canvas;
+    GameObject score;
+    GameObject hiscore;
     int scoreCount = 0;
 
     void Awake(){
-        scoreTxt = GetComponent<TextMeshProUGUI>();
+        //scoreTxt = GetComponent<TextMeshProUGUI>();
+        canvas = GameObject.Find("Canvas");
+        score = canvas.transform.GetChild(1).gameObject;
+        hiscore = canvas.transform.GetChild(3).gameObject;
+        scoreTxt = score.GetComponent<TextMeshProUGUI>();
+        highScoreTxt = hiscore.GetComponent<TextMeshProUGUI>();
     }
 
     void Start(){
-        
+        highScoreTxt.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
 
     public void ScoreIncrease(int val){
         scoreCount += val;
         scoreTxt.text = scoreCount.ToString();
+
+        if(scoreCount > PlayerPrefs.GetInt("HighScore", 0)){
+            PlayerPrefs.SetInt("HighScore", scoreCount);
+            highScoreTxt.text = scoreCount.ToString();
+        }
+        
+    }
+
+    public void Reset(){
+        PlayerPrefs.DeleteKey("HighScore");
+        highScoreTxt.text = "0";
     }
 
 }
